f5308319c23baf06e3a3e180773a59100a73f9a8
Adam Howard <achdisposable1+github@gmail.com>
2019-11-13 14:50:18 +0000

    refactor(Loader): replace JUniversalChardet w/ internal charset detector
    JUniversalChardet has a bug where it doesn't detect the encoding of ASCII files

--- src/main/kotlin/com/github/medavox/kotban/Loader.kt
+++ src/main/kotlin/com/github/medavox/kotban/Loader.kt
@@ -1,14 +1,15 @@
 package com.github.medavox.kotban
 
-import org.mozilla.universalchardet.ReaderFactory
-import org.mozilla.universalchardet.UniversalDetector
 import java.io.File
 
 object Loader {
-    private val detector:UniversalDetector = UniversalDetector()
     fun load(dir: File):Board {
         if(!dir.isDirectory) {
             throw Exception("supplied argument must be a directory")
+        }
+
+        if(!dir.canRead()) {
+            throw Exception("can't read input path. Probably no permission")
         }
 
         val dirsInDir:List<File> = dir.listFiles()?.filter { it?.isDirectory ?: false } ?: listOf<File>()
@@ -17,29 +18,24 @@ object Loader {
         if(dirsInDir.isEmpty()) {
             throw Exception("no bin subdirs in board dir. Exiting.")
         }
-        val subDirsAndTheirItems:Map<String, List<Item>> = dirsInDir.associate {
-            it.name to it.listFiles().filter {file:File ->
+        val subDirsAndTheirItems:Map<String, List<Item>> = dirsInDir.associate {subdir ->
+            //println("stuff in '$it': ${Arrays.toString(it.listFiles())}")
+            subdir.name to (subdir.listFiles() ?: arrayOf()).filter {file:File ->
+                //println("file in $subdir: $file")
                 //filter out non-files, unreadables, nonexistent, and files >10MB
-                it.isFile && it.canRead() && it.exists() && it.length() < (10240 * 1024)
-            }.filter { file ->
-                val encoding:String? = UniversalDetector.detectCharset(file)
-                /*detector.reset()
-                detector.handleData(file.readBytes())
-                detector.dataEnd()
-
-                detector.detectedCharset != null*/
+                file.isFile && file.canRead() && file.exists() && file.length() < (10240 * 1024)
+            }.associateWith { foil ->
+                //detect charsets
+                CharsetDetector.detectCharset(foil)
+            }.filter { (file, encoding) ->
+                //filter out files with undetected charsets, ie non-text files
+                println("encoding of $file: $encoding")
                 encoding != null
-                //fixme: the UniversalCharsetDetector, can actually detect a wider range of charsets
-                // than Kotlin/Java can decode.
-                // so we need to limit 'valid' charset outputs to the following:
-                //US-ASCII 	Seven-bit ASCII, a.k.a. ISO646-US, a.k.a. the Basic Latin block of the Unicode character set
-                //ISO-8859-1   	ISO Latin Alphabet No. 1, a.k.a. ISO-LATIN-1
-                //UTF-8 	Eight-bit UCS Transformation Format
-                //UTF-16BE 	Sixteen-bit UCS Transformation Format, big-endian byte order
-                //UTF-16LE 	Sixteen-bit UCS Transformation Format, little-endian byte order
-                //UTF-16 	Sixteen-bit UCS Transformation Format, byte order identified by an optional byte-order mark
-            }.map { fle ->  Item(title = fle.name, contents = ReaderFactory.createBufferedReader(fle).readText())}
+            }.map { (file, charset) ->
+                Item(title = file.name, contents = file.readText(charset!!))
+            }
         }
+        println("panes: $subDirsAndTheirItems")
         return Board(name=dir.name, panes=subDirsAndTheirItems)
     }
 }
