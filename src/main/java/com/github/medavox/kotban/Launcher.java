5faff496374d714f685f3fbc5e78b7269a9c20a4
Adam Howard <achdisposable1+github@gmail.com>
2019-11-15 12:02:03 +0000

    stop using TornadoFX, and just use plain JavaFX
    the DSL syntax was too confusing and undiscoverable.
    Felt like writing gradle code, which not a complement.
    Is it a function? property? closure???
    
    Plus, there are way more examples for JavaFX,
    which are much more easily translatable from java to kotlin,
    than from JavaFX to TornadoFX.

---
+++ src/main/java/com/github/medavox/kotban/Launcher.java
@@ -0,0 +1,5 @@
+package com.github.medavox.kotban;
+
+import javafx.application.Application;
+
+public class Launcher { public static void main(String[] args) { Application.launch(Gui.class, ""); } }
--- src/main/kotlin/com/github/medavox/kotban/Gui.kt
+++ src/main/kotlin/com/github/medavox/kotban/Gui.kt
@@ -1,10 +1,22 @@
 package com.github.medavox.kotban
 
+import javafx.application.Application
 import javafx.geometry.Orientation
 import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.TextArea
+import javafx.scene.control.TitledPane
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.HBox
+import javafx.stage.Stage
 import tornadofx.*
 import tornadofx.Stylesheet.Companion.datagrid
 import java.io.File
+import javafx.scene.layout.VBox
+
+
 
 //some more tutorial & learning resources;
 //https://docs.oracle.com/javase/8/javafx/get-started-tutorial/form.htm#CFHEAHGB
@@ -13,96 +25,82 @@ import java.io.File
  * Implemented with JavaFX, available as part of Java 8's language API.
  * Note that after Java 8, JavaFX was made an external library.
  * @see [https://docs.oracle.com/javase/8/javafx/api](JavaFX javadoc)*/
-class Gui : View() {
+class Gui : Application() {
     //input = multi-line text input field
     //language choice = dropdown list
     //result = multi-line text output field
     //errors = multi-line text output field
 
-    val board = Loader.load(File("./testboard"))
-    override val root: Parent = flowpane {
-        title = board.name
+    /*Instead of making entries editable (andd effedctively having to write our own text editor),
+    * make each entry, upon being clicked, open itself in the user's choice of editor.
+    * That allows us to focus on making thw Markdown pretty, too*/
+    /*override val root: Parent = flowpane {
         orientation = Orientation.VERTICAL
         //label(board.name)
         for((name, entries) in board.panes) {
-            /*panes*/hbox{
+            *//*panes*//*hbox{
                 label(name)//todo: do the label some other way
-                scrollpane {
-                    content=vbox {
-                        for (entry in entries) {
-                            titledpane {
-                                text = entry.title
-                                content = textarea { text = entry.contents }
+                anchorpane {
+                    anchorpaneConstraints{ AnchorPaneConstraint(0.0, 0.0, 0.0, 0.0)}
+                    scrollpane(fitToWidth = true) {
+                        //useMaxHeight = true
+                        //maxHeight = 100.0
+                        vbarPolicy = ScrollPane.ScrollBarPolicy.ALWAYS
+                        vbox {
+                            for (entry in entries) {
+                                titledpane {
+                                    text = entry.title
+                                    content = textarea { text = entry.contents }
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
-/*
+    }*/
+
     override fun start(primaryStage:Stage) {
-        val root = GridPane()
+        /*val root = GridPane()
         root.setAlignment(Pos.CENTER)
         root.setHgap(10.0); root.setVgap(10.0)
-        root.setPadding(Insets(10.0, 10.0, 10.0, 10.0))
-
-        val input = TextArea()
-        input.setEditable(true)
-        input.promptText = UiStrings.inputHint
-        input.setWrapText(true)
-
-        val output = TextArea()
-        output.promptText = UiStrings.outputHint
-        output.setEditable(false)
-        output.setWrapText(true)
-
-        val errors = TextArea()
-        errors.promptText = UiStrings.errorsHint
-        errors.setEditable(false)
-        errors.setWrapText(true)
-        errors.text
-
-        val menuButton = MenuButton()
-        var selection: Language = Language.ENGLISH
-        val languagesMenuItems:Array<MenuItem> = Language.values().
-            map {
-                val m = MenuItem(it.neim)
-                m.setOnAction { event ->
-                    menuButton.text = it.neim
-                    selection = it
-                }
-                m
-            }.toTypedArray()
-        menuButton.items.addAll(*languagesMenuItems)
+        root.setPadding(Insets(10.0, 10.0, 10.0, 10.0))*/
 
         //val out = PrintStreamCapturer(output, System.out)
-        System.setErr(err)
+        val root = HBox()
+        root.setPrefSize(600.0, 600.0)
 
-        val btn = Button()
-        btn.setText("Transliterate")
-        btn.alignment = Pos.CENTER_RIGHT
-        btn.setOnAction { event:ActionEvent ->
-            System.out.println("transcribing with $selection")
-            errors.text = ""
-            output.text = selection.transcriber.transcribe(input.text)
-            //err.println("error text")
-            //out.println("output text")
+        System.setErr(System.err)
+        val board = Loader.load(File("./testboard"))
+        for((name, entries) in board.panes) {
+            //label(name)//todo: do the label some other way
+            //root.children.add()
+            val anchorPane = AnchorPane()
+            val scrollPane = ScrollPane()
+            val vb2 = VBox()
+
+            AnchorPane.setTopAnchor(scrollPane, 0.0)
+            AnchorPane.setBottomAnchor(scrollPane, 0.0)
+            AnchorPane.setLeftAnchor(scrollPane, 0.0)
+            AnchorPane.setRightAnchor(scrollPane, 0.0)
+            anchorPane.children.add(scrollPane)
+            //Add content ScrollPane
+            scrollPane.content = vb2
+            for (entry in entries) {
+                vb2.children.add(TitledPane(entry.title, TextArea(entry.contents)))
+            }
+            root.children.add(anchorPane)
         }
 
 
-        root.add(input, 0, 0, 2, 1)
+/*        root.add(input, 0, 0, 2, 1)
         root.add(menuButton, 0, 1)
         root.add(btn, 1, 1)
         root.add(output, 0, 3, 2, 1)
-        root.add(errors, 0, 4, 2, 1)
+        root.add(errors, 0, 4, 2, 1)*/
 
-        val scene = Scene(root, 400.0, 400.0)
-
-        primaryStage.setTitle("Kotban")
-        primaryStage.setScene(scene)
+        primaryStage.title = board.name+" - Kotban"
+        primaryStage.scene = Scene(root, 400.0, 400.0)
         primaryStage.show()
     }
-
- */
 }
