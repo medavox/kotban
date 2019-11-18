fa4520064be2c15905ce8e536a8cfbb770c56903
Adam Howard <achdisposable1+github@gmail.com>
2019-11-18 11:37:06 +0000

    refactor(DragResizerXY): convert to Kotlin

---
+++ src/main/kotlin/com/github/medavox/kotban/DragResizerXY.kt
@@ -0,0 +1,145 @@
+package com.github.medavox.kotban
+
+import javafx.event.EventHandler
+import javafx.scene.Cursor
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.Region
+
+/**
+ * The margin around the control that a user can click in to start resizing
+ * the region.
+ */
+private const val RESIZE_MARGIN = 10
+
+/**
+ * {@link DragResizerXY} can be used to add mouse listeners to a {@link Region}
+ * and make it resizable by the user by clicking and dragging the border in the
+ * same way as a window.
+ * <p>
+ * Height and Width resizing is working (hopefully) properly
+ * 
+ * <pre>
+ * DragResizer.makeResizable(myAnchorPane)
+ * </pre>
+ *
+ * @author Cannibalsticky (modified from the original DragResizer created by AndyTill)
+ *
+ */
+class DragResizerXY private constructor(private val region:Region ) {
+
+	private var y:Double=0.0
+	private var x:Double=0.0
+	private var initMinHeight:Boolean=false
+	private var initMinWidth:Boolean=false
+	private var draggableZoneX:Boolean=false
+	private var draggableZoneY:Boolean=false
+	private var dragging:Boolean=false
+
+
+
+	fun makeResizable(region:Region) {
+		region.setOnMousePressed(object:EventHandler<MouseEvent> {
+			override fun handle(event:MouseEvent) {
+				mousePressed(event)
+			}
+		})
+		region.setOnMouseDragged(object:EventHandler<MouseEvent> {
+			override fun handle(event:MouseEvent) {
+				mouseDragged(event)
+			}
+		})
+		region.setOnMouseMoved(object:EventHandler<MouseEvent> {
+			override fun handle(event:MouseEvent) {
+				mouseOver(event)
+			}
+		})
+		region.setOnMouseReleased(object:EventHandler<MouseEvent> {
+			override fun handle(event:MouseEvent) {
+				mouseReleased(event)
+			}
+		})
+	}
+
+	protected fun mouseReleased(event:MouseEvent) {
+		dragging = false
+		region.setCursor(Cursor.DEFAULT)
+	}
+
+	protected fun mouseOver(event:MouseEvent) {
+		if (isInDraggableZone(event) || dragging) {
+			if (draggableZoneY) {
+				region.setCursor(Cursor.S_RESIZE)
+			}
+
+			if (draggableZoneX) {
+				region.setCursor(Cursor.E_RESIZE)
+			}
+
+		} else {
+			region.setCursor(Cursor.DEFAULT)
+		}
+	}
+	
+	
+	/**Had to use 2 variables for the control.
+	Tried without, had unexpected behaviour (going big was ok, going small nope.)*/
+	protected fun isInDraggableZone(event:MouseEvent):Boolean {
+		draggableZoneY = (event.getY() > (region.getHeight() - RESIZE_MARGIN))
+		draggableZoneX = (event.getX() > (region.getWidth() - RESIZE_MARGIN))
+		return (draggableZoneY || draggableZoneX)
+	}
+
+	protected fun mouseDragged(event:MouseEvent) {
+		if (!dragging) {
+			return
+		}
+
+		if (draggableZoneY) {
+			val mousey = event.getY()
+
+			val newHeight = region.getMinHeight() + (mousey - y)
+
+			region.setMinHeight(newHeight)
+
+			y = mousey
+		}
+
+		if (draggableZoneX) {
+			val mousex = event.getX()
+
+			val newWidth = region.getMinWidth() + (mousex - x)
+
+			region.setMinWidth(newWidth)
+
+			x = mousex
+
+		}
+
+	}
+
+	protected fun mousePressed(event:MouseEvent) {
+		// ignore clicks outside of the draggable margin
+		if (!isInDraggableZone(event)) {
+			return
+		}
+
+		dragging = true
+
+		// make sure that the minimum height is set to the current height once,
+		// setting a min height that is smaller than the current height will
+		// have no effect
+		if (!initMinHeight) {
+			region.setMinHeight(region.getHeight())
+			initMinHeight = true
+		}
+
+		y = event.getY()
+
+		if (!initMinWidth) {
+			region.setMinWidth(region.getWidth())
+			initMinWidth = true
+		}
+
+		x = event.getX()
+	}
+}
