e3a55298e1a672aab1245c5e0e283d2d56144552
Adam Howard <achdisposable1+github@gmail.com>
2019-12-07 09:20:03 +0000

    refactor(Launcher):split into multiple lines
    it's easier when I want to add stuff as a test

--- src/main/java/com/github/medavox/kotban/Launcher.java
+++ src/main/java/com/github/medavox/kotban/Launcher.java
@@ -2,4 +2,8 @@ package com.github.medavox.kotban;
 
 import javafx.application.Application;
 
-public class Launcher { public static void main(String[] args) { Application.launch(Gui.class, ""); } }
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Gui.class, "");
+    }
+}
